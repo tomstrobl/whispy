834c8cd23d2232125fe1b33375d6d177ebeec389
Aron Ventura <loki.aron321@gmail.com>
2026-07-03 20:14:12 +0200

    Staircaseplot hinzugefügt

--- whispy/utils/__init__.py
+++ whispy/utils/__init__.py
@@ -1,9 +1,10 @@
 from .utils import read_config, load_design
-from .results import participant_id_from_consent, save_results
+from .results import participant_id_from_consent, save_results, plot_staircase
 
 __all__ = [
     'read_config',
     'load_design',
     'participant_id_from_consent',
     'save_results',
+    'plot_staircase',
 ]
